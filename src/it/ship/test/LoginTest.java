2e3d385dc3caa7603713dba4de78b634c339fb3a
Luca Moggio <lucamoggio6@gmail.com>
2022-05-25 19:52:31 +0200

    Update LoginTest.java

--- src/it/ship/test/LoginTest.java
+++ src/it/ship/test/LoginTest.java
@@ -9,6 +9,9 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -16,11 +19,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import it.ship.beans.Cliente;
+import it.ship.servlets.gestioneSistema.DriverManagerConnectionPool;
 import it.ship.servlets.gestioneUtente.LogIn;
 
 class LoginTest {
@@ -39,14 +45,33 @@ class LoginTest {
 	 * Prima di ogni test simula la sessione dell'utente.
 	 */
 	@BeforeEach
-	public void setUp() {
+	public void setUp() throws SQLException {
 		when(requestMock.getSession()).thenReturn(sessionMock);
-	
+		Connection conn = DriverManagerConnectionPool.getConnection();
+		Statement stmtSelect = conn.createStatement();
+		stmtSelect.executeUpdate("insert INTO  cliente (nome,cognome,datanascita,username,password,codicefiscale,email) VALUES('Mario','Rossi','2022-01-15','mario','passwordTest','ZCXZVJ86E03L710J','mariorossi@test.com');");
+		conn.close();
 		
+	}
+	
+	/*
+	 * Dopo ogni test elimino l'utente appena creato.
+	 */
+	@AfterEach
+	void tearDown() {
+		try {
+			Connection conn = DriverManagerConnectionPool.getConnection();
+			Statement stmtSelect = conn.createStatement();
+			stmtSelect.executeUpdate("DELETE FROM cliente WHERE CodiceFiscale='ZCXZVJ86E03L710J';");
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	void testTrue() throws ServletException, IOException {
+
 		doReturn(mockedServletContext).when(requestMock).getServletContext();
 		when(requestMock.getParameter("user")).thenReturn("mario");
 		when(requestMock.getParameter("pass")).thenReturn("passwordTest");
