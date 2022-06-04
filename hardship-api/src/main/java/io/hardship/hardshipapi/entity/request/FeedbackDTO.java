d43819c4f85ae653c4ddba84c963c618c8a654b1
ChristianGiuseppe <nocerino.christian.cn@gmail.com>
2022-06-04 10:39:47 +0200

    changed DTO

--- hardship-api/src/main/java/io/hardship/hardshipapi/entity/request/FeedbackDTO.java
+++ hardship-api/src/main/java/io/hardship/hardshipapi/entity/request/FeedbackDTO.java
@@ -1,14 +1,12 @@
 package io.hardship.hardshipapi.entity.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class FeedbackDTO {
     private String titolo;
     private String testo;
