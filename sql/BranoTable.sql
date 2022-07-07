f3ba24a4d7ab445abdd9623417b10cce5a202557
Luca Moggio <36155626+lukeno995@users.noreply.github.com>
2022-07-08 01:22:47 +0200

    add files

---
+++ sql/BranoTable.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM hardship.brano
+LIMIT 0, 1000
+
+-- Date: 2022-07-08 01:20
+*/
+INSERT INTO `` (`ID`,`Titolo`,`Anno`,`Durata`,`traccia`,`ID_album`,`ID_artista`) VALUES (1,'Hollywood’s Bleeding','2019','2:36','http://localhost:8080/files/HollywoodsBleeding.mp3',5,1);
+INSERT INTO `` (`ID`,`Titolo`,`Anno`,`Durata`,`traccia`,`ID_album`,`ID_artista`) VALUES (2,'Saint-Tropez','2019','2:30','http://localhost:8080/files/Saint-Tropez.mp3',5,1);
+INSERT INTO `` (`ID`,`Titolo`,`Anno`,`Durata`,`traccia`,`ID_album`,`ID_artista`) VALUES (3,'Enemies','2019','3:16','http://localhost:8080/files/Enemies.mp3',5,1);
